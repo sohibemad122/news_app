3e01f31e9c9ac46142461562c2af0b9936765722
sohibemad122 <m9749172@gmail.com>
2025-09-04 18:09:47 +0300

    constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  static const String apiKey = "5f71503614be43d3a9fcdd5a71fa656a";
+  static const String apiKey = "PUT-YOUR-API-KEY-HERE";
   static const String baseUrl = "https://newsapi.org/v2";
 }
